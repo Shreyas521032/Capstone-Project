613cbdce5f216971fcdc55b241c0dc9898de5d6a
Shreyas Kasture <149940538+Shreyas521032@users.noreply.github.com>
2025-04-03 00:22:16 +0530

    Update Capstone Project.py

--- Project/Capstone Project.py
+++ Project/Capstone Project.py
@@ -61,7 +61,7 @@ st.markdown("<h1 class='main-header'>Healthcare Data Analytics Platform</h1>", u
 @st.cache_data
 def load_data():
     try:       
-        return pd.read_excel("Datasets/cancer_website_survey_dataset.xlsx")          
+        return pd.read_excel("Project/cancer_survey_dataset.xlsx")          
     except Exception as e:
         st.error(f"Error loading dataset: {str(e)}")
         st.stop()  # This will halt the app if data fails to load 
