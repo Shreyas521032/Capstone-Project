09caf9c2e7accb4022a73780a01fda578f2d0eaa
Shreyas Kasture <149940538+Shreyas521032@users.noreply.github.com>
2025-04-12 13:06:36 +0530

    Update app.py

--- Project/app.py
+++ Project/app.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error, accuracy_score, classification_report, f1_score, mean_absolute_error
 import plotly.express as px
 import plotly.graph_objects as go
-from sklearn.preprocessing import StandardScalera
+from sklearn.preprocessing import StandardScaler
 
 # Set page configuration
 st.set_page_config(
